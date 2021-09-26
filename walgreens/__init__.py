1d2941489f741ee0da42a60d9036545b183f72a2
btxuyenhcmus <btxuyenhcmus@gmail.com>
2021-09-26 20:35:15 +0700

    [ADD] walgreens

---
+++ walgreens/__init__.py
@@ -0,0 +1,1 @@
+from .walgreens import Walgreens
--- walgreens/walgreens.py
+++ walgreens/walgreens.py
@@ -3,28 +3,28 @@ from base import Base
 import os
 
 pathfile = os.path.dirname(os.path.realpath(__file__))
-DNS_WEB = "https://www.colehaan.com"
+DNS_WEB = "https://www.walgreens.com"
 
 
-class Colehaan(Base):
+class Walgreens(Base):
     eP = Extractor.from_yaml_file("{}/selector_product.yml".format(pathfile))
     __instance = None
 
     def __init__(self, dns=DNS_WEB) -> None:
-        if Colehaan.__instance != None:
+        if Walgreens.__instance != None:
             raise Exception("This is singleton class!!")
         self.dns = dns
-        Colehaan.__instance = self
+        Walgreens.__instance = self
 
     @staticmethod
     def getInstance() -> object:
         """This is static method be called by class"""
-        if Colehaan.__instance == None:
-            Colehaan()
-        return Colehaan.__instance
+        if Walgreens.__instance == None:
+            Walgreens()
+        return Walgreens.__instance
 
     def product(self, url) -> dict:
-        return Colehaan.eP.extract(super().product(url))
+        return Walgreens.eP.extract(super().product(url))
 
     def __str__(self) -> str:
-        return "Colehaan Model"
+        return "Walgreens Model"
