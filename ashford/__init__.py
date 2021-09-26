eec1ca742e1ef190d1b7c219fe99f039fc96b742
btxuyenhcmus <btxuyenhcmus@gmail.com>
2021-09-26 23:28:30 +0700

    [ADD] ashford

---
+++ ashford/__init__.py
@@ -0,0 +1,1 @@
+from .ashford import Ashford
---
+++ ashford/ashford.py
@@ -0,0 +1,30 @@
+from selectorlib import Extractor
+from base import Base
+import os
+
+pathfile = os.path.dirname(os.path.realpath(__file__))
+DNS_WEB = "https://www.ashford.com"
+
+
+class Ashford(Base):
+    eP = Extractor.from_yaml_file("{}/selector_product.yml".format(pathfile))
+    __instance = None
+
+    def __init__(self, dns=DNS_WEB) -> None:
+        if Ashford.__instance != None:
+            raise Exception("This is singleton class!!")
+        self.dns = dns
+        Ashford.__instance = self
+
+    @staticmethod
+    def getInstance() -> object:
+        """This is static method be called by class"""
+        if Ashford.__instance == None:
+            Ashford()
+        return Ashford.__instance
+
+    def product(self, url) -> dict:
+        return Ashford.eP.extract(super().product(url))
+
+    def __str__(self) -> str:
+        return "Ashford Model"
