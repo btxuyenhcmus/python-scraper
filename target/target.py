3c0a5209a7a67fd32f3458b72619e3b32d706edf
btxuyenhcmus <btxuyenhcmus@gmail.com>
2021-09-28 12:28:09 +0700

    [ADD] added target

---
+++ target/target.py
@@ -0,0 +1,33 @@
+from selectorlib import Extractor
+from base import Base, RESP_DEFAULT
+import os
+
+pathfile = os.path.dirname(os.path.realpath(__file__))
+DNS_WEB = "https://www.target.com"
+
+
+class Target(Base):
+    eP = Extractor.from_yaml_file("{}/selector_product.yml".format(pathfile))
+    __instance = None
+
+    def __init__(self, dns=DNS_WEB) -> None:
+        if Target.__instance != None:
+            raise Exception("This is singleton class!!")
+        self.dns = dns
+        Target.__instance = self
+
+    @staticmethod
+    def getInstance() -> object:
+        """This is static method be called by class"""
+        if Target.__instance == None:
+            Target()
+        return Target.__instance
+
+    def product(self, url) -> dict:
+        try:
+            return Target.eP.extract(super().product(url))
+        except Exception as e:
+            return RESP_DEFAULT
+
+    def __str__(self) -> str:
+        return "Target Model"
