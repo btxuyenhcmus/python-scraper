225a9710f6cddea04465dbbebd55e4827f1fb94f
btxuyenhcmus <btxuyenhcmus@gmail.com>
2021-10-03 13:04:39 +0700

    [IMP] adidas: improve adidas

--- adidas/adidas.py
+++ adidas/adidas.py
@@ -1,5 +1,8 @@
 from selectorlib import Extractor
+from werkzeug.utils import html
 from base import Base, RESP_DEFAULT
+from bs4 import BeautifulSoup
+from flask import json
 import os
 
 pathfile = os.path.dirname(os.path.realpath(__file__))
@@ -25,7 +28,20 @@ class Adidas(Base):
 
     def product(self, url) -> dict:
         try:
-            return Adidas.eP.extract(super().product(url))
+            html = super().product(url)
+            soup = BeautifulSoup(html, 'lxml')
+            data = json.loads(
+                str(soup.find('script', type='application/ld+json'))[50:-9])
+            resp = Adidas.eP.extract(html)
+            resp.update({
+                'name': data["name"],
+                'price': data["offers"]["price"],
+                'image': data["image"][0],
+                'number_of_reviews': data["aggregateRating"]["reviewCount"],
+                'rating': data["aggregateRating"]["ratingValue"],
+                'product_description': data["description"]
+            })
+            return resp
         except Exception as e:
             return RESP_DEFAULT
 
