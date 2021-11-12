dda5b1c0ba834cff007617c5755998b66f754973
btxuyenhcmus <btxuyenhcmus@gmail.com>
2021-11-13 02:01:37 +0700

    [FIX] overstock

--- scraper/overstock/overstock.py
+++ scraper/overstock/overstock.py
@@ -1,17 +1,17 @@
-import logging
 from selectorlib import Extractor
-import requests
-from base import Base, RESP_DEFAULT
+from base import RESP_DEFAULT, ChromePath
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+import logging
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 pathfile = os.path.dirname(os.path.realpath(__file__))
 DNS_WEB = "https://www.overstock.com"
 
 
-class Overstock(Base):
+class Overstock():
     eP = Extractor.from_yaml_file("{}/selector_product.yml".format(pathfile))
     __instance = None
 
@@ -28,27 +28,45 @@ class Overstock(Base):
             Overstock()
         return Overstock.__instance
 
+    @property
+    def headers(self) -> dict:
+        return {
+            'dnt': '1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'sec-fetch-site': 'same-origin',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-user': '?1',
+            'sec-fetch-dest': 'document',
+            'referer': self.dns,
+            'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+        }
+
+    @property
+    def UserAgent(self):
+        return f'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36'
+
     def product(self, url) -> dict:
+        logging.info("Downloading {}".format(url))
+        options = webdriver.ChromeOptions()
+        options.add_argument("headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument(
+            "--proxy-server={}".format(os.getenv('HTTP_PROXY')))
+        options.add_argument(self.UserAgent)
         try:
-            logging.info("Downloading {}".format(url))
-            proxyDict = {
-                'http': os.getenv('HTTP_PROXY'),
-                'https': os.getenv('HTTPS_PROXY')
-            }
-            r = requests.get(url, headers=self.headers,
-                             timeout=10, proxies=proxyDict)
-            if r.status_code > 500:
-                if "To discuss automated access to Website data please contact" in r.text:
-                    print(
-                        "Page %s was blocked by Website. Please try using better proxies\n" % url)
-                else:
-                    print("Page %s must have been blocked by Website as the status code was %d" % (
-                        url, r.status_code))
-                return {}
-            return Overstock.eP.extract(r.text)
+            browser = webdriver.Chrome(ChromePath, options=options)
+            browser.get(url)
+            content = browser.page_source
         except Exception as e:
             logging.error(e)
-            return RESP_DEFAULT
+        browser.close()
+        try:
+            return Overstock.eP.extract(content)
+        except Exception as e:
+            logging.error(e)
+        return RESP_DEFAULT
 
     def __str__(self) -> str:
         return "Overstock Model"
