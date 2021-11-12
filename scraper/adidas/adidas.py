0daf4c0142e7cd59a67ff529ba858eec732dbaa6
btxuyenhcmus <btxuyenhcmus@gmail.com>
2021-11-13 01:48:12 +0700

    [FIX] adidas

--- scraper/adidas/adidas.py
+++ scraper/adidas/adidas.py
@@ -1,16 +1,14 @@
-import logging
 from selectorlib import Extractor
-from werkzeug.utils import html
-from base import Base, RESP_DEFAULT
-from bs4 import BeautifulSoup
-from flask import json
+from base import RESP_DEFAULT, ChromePath
+from selenium import webdriver
+import logging
 import os
 
 pathfile = os.path.dirname(os.path.realpath(__file__))
 DNS_WEB = "https://www.adidas.com"
 
 
-class Adidas(Base):
+class Adidas():
     eP = Extractor.from_yaml_file("{}/selector_product.yml".format(pathfile))
     __instance = None
 
@@ -27,28 +25,47 @@ class Adidas(Base):
             Adidas()
         return Adidas.__instance
 
+    @property
+    def headers(self) -> dict:
+        return {
+            'dnt': '1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'sec-fetch-site': 'same-origin',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-user': '?1',
+            'sec-fetch-dest': 'document',
+            'referer': self.dns,
+            'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+        }
+
+    @property
+    def UserAgent(self):
+        return f'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36'
+
     def product(self, url) -> dict:
+        logging.info("Downloading {}".format(url))
+        options = webdriver.ChromeOptions()
+        options.add_argument("headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument(self.UserAgent)
         try:
-            html = super().product(url)
-            soup = BeautifulSoup(html, 'lxml')
-            data = json.loads(
-                str(soup.find('script', type='application/ld+json'))[50:-9])
-            resp = Adidas.eP.extract(html)
-            try:
-                resp.update({
-                    'name': data["name"],
-                    'price': data["offers"]["price"],
-                    'image': data["image"][0],
-                    'number_of_reviews': data["aggregateRating"]["reviewCount"],
-                    'rating': data["aggregateRating"]["ratingValue"],
-                    'product_description': data["description"]
-                })
-            except Exception as e:
-                logging.error(e)
+            browser = webdriver.Chrome(ChromePath, options=options)
+            browser.get(url)
+            content = browser.page_source
+        except Exception as e:
+            logging.error(e)
+        browser.close()
+        try:
+            resp = Adidas.eP.extract(content)
+            resp.update({
+                'price': resp["price_sale"] or resp["price"]
+            })
             return resp
         except Exception as e:
             logging.error(e)
-            return RESP_DEFAULT
+        return RESP_DEFAULT
 
     def __str__(self) -> str:
         return "Adidas Model"
